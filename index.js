144f4d380b082ee8b1f1a96f49e460586279ccaa
Mohamad Fakhouri <mohahf19@gmail.com>
2023-02-24 01:12:00 +0100

    added await

--- index.js
+++ index.js
@@ -3,52 +3,55 @@ const github = require("@actions/github");
 const asana = require("asana");
 
 const asanaHomeLink = "https://app.asana.com/0/";
-try {
-    // Get the github token
-    const githubToken = core.getInput("GITHUB_TOKEN");
-    // Get the Asana key and project
-    const asanaPAT = core.getInput("ASANA_TOKEN");
-    const asanaProjectGid = core.getInput("ASANA_PROJECT_GID");
 
-    // get the PR name
-    const prName = github.context.payload.pull_request.title;
-    const { context = {} } = github;
-    const { pull_request } = context.payload;
-    // get the ticket number, which is in the format "[TICKET-NUMBER] more text"
-    // find first occurance of a square bracket
-    const ticketNumber = prName.substring(0, prName.indexOf("]") + 1);
+async function run() {
+    try {
+        // Get the github token
+        const githubToken = core.getInput("GITHUB_TOKEN");
+        // Get the Asana key and project
+        const asanaPAT = core.getInput("ASANA_TOKEN");
+        const asanaProjectGid = core.getInput("ASANA_PROJECT_GID");
 
-    //initialize asana client
-    const client = asana.Client.create().useAccessToken(asanaPAT);
-    client.tasks
-        .getTasksForProject(asanaProjectGid, { opt_pretty: true })
-        .then((result) => {
-            // find the task that starts with the ticketNumer (there is at most one)
-            const index = result.data.findIndex((x) =>
-                x.name.startsWith(ticketNumber)
-            );
+        // get the PR name
+        const prName = github.context.payload.pull_request.title;
+        const { context = {} } = github;
+        const { pull_request } = context.payload;
+        // get the ticket number, which is in the format "[TICKET-NUMBER] more text"
+        // find first occurance of a square bracket
+        const ticketNumber = prName.substring(0, prName.indexOf("]") + 1);
 
-            if (index > -1) {
-                const task = result.data[index];
-                console.log("Task found: " + task.name);
-                const taskGid = task.gid;
-                const taskLink = asanaHomeLink + asanaProjectGid + "/" + taskGid;
-                console.log("Task link: " + taskLink);
-
-                // add comment to the PR wit the task link and task name
-                const octokit = github.getOctokit(githubToken);
-                octokit.rest.issues.createComment({
-                    ...context.repo,
-                    issue_number: pull_request.number,
-                    body: "Asana Task: [" + task.name + "](" + taskLink + ")",
-                });
-            } else {
-                throw new Error("No task found for ticket number: " + ticketNumber);
-            }
+        //initialize asana client
+        const client = asana.Client.create().useAccessToken(asanaPAT);
+        const result = client.tasks.getTasksForProject(asanaProjectGid, {
+            opt_pretty: true,
         });
+        // find the task that starts with the ticketNumer (there is at most one)
+        const index = result.data.findIndex((x) => x.name.startsWith(ticketNumber));
 
-    console.log(`Ticket Number: ${ticketNumber}`);
-} catch (error) {
-    console.log(error);
-    console.log("No task found");
+        if (index > -1) {
+            const task = result.data[index];
+            console.log("Task found: " + task.name);
+            const taskGid = task.gid;
+            const taskLink = asanaHomeLink + asanaProjectGid + "/" + taskGid;
+            console.log("Task link: " + taskLink);
+
+            // add comment to the PR wit the task link and task name
+            const octokit = github.getOctokit(githubToken);
+            await octokit.rest.issues.createComment({
+                ...context.repo,
+                issue_number: pull_request.number,
+                body: "Asana Task: [" + task.name + "](" + taskLink + ")",
+            });
+            ockto;
+        } else {
+            throw new Error("No task found for ticket number: " + ticketNumber);
+        }
+
+        console.log(`Ticket Number: ${ticketNumber}`);
+    } catch (error) {
+        console.log(error);
+        console.log("No task found");
+    }
 }
+
+run();
